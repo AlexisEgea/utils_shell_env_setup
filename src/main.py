5a5c57bdef24b5a20be40d732fafb0ab1eb36486
AlexisEgea <alexisegea@oulook.com>
2026-08-13 16:13:11 +0200

    Adding main which will reproduction 'pip freeze' behaviour for only librairies on requirements.txt file

--- src/main.py
+++ src/main.py
@@ -1,15 +1,26 @@
-import os 
-from pathlib import Path
-from dotenv import load_dotenv
+from utils.env_detection import detect_virtual_environment
+from utils.env_list_detection import detect_required_libraries_env, detect_installed_libraries
 
 if __name__ == "__main__":
-    env_path = Path(__file__).resolve().parents[1] / "infra" / "env"
-    loaded = load_dotenv(dotenv_path=env_path)
+    dict_env = detect_virtual_environment()
+    print("Detected environment:")
+    for key, value in dict_env.items():
+        print(f"{key}: {value}")
 
-    print(f"Hello {os.getenv('USER') }!")
+    required_libraries_env = detect_required_libraries_env()
+    print("\nRequired libraries in env:")
+    for package_name in required_libraries_env:
+        print(f"- {package_name}")
 
-    # Build a program that checks library versions installed in the venv.
-    # Only retrieve versions for libraries listed in requirements.txt.
-    # The program must display versions of libraries installed in the venv.
-    # The program must display versions of libraries listed in requirements.txt.
-    # Apply the same logic based on detected virtual environment: venv or conda.
+    installed_libraries = detect_installed_libraries(dict_env["active_environment"])
+    for package_name, package_version in installed_libraries.items():
+        print(f"- {package_name}=={package_version}")
+
+    required_librairies_with_version: dict[str, str] = {}
+    for package_name in required_libraries_env:
+        if package_name not in required_librairies_with_version and package_name in installed_libraries.keys():
+            required_librairies_with_version[package_name] = installed_libraries[package_name]
+    
+    print("\nRequired libraries with version for requirements.txt:")
+    for package_name, package_version in required_librairies_with_version.items():
+        print(f"- {package_name}=={package_version}")
